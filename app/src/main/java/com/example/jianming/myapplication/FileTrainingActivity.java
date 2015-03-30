68ddec9b420c19b2915a87578c88969d2e3e5bca
knightingal <yujiaming1985@hotmail.com>
2015-03-30 22:49:48 +0800

    file training

---
+++ app/src/main/java/com/example/jianming/myapplication/FileTrainingActivity.java
@@ -0,0 +1,93 @@
+package com.example.jianming.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class FileTrainingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_training);
+        Button writeButton = (Button) findViewById(R.id.write_file);
+        writeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                writeFile();
+            }
+        });
+        Button readButton = (Button) findViewById(R.id.read_file);
+        readButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readFile();
+            }
+        });
+    }
+
+    private void writeFile() {
+        String fileName = "myfile";
+        String string = "Hello World";
+        FileOutputStream fileOutputStream;
+
+        try {
+            fileOutputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+            fileOutputStream.write(string.getBytes());
+            fileOutputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void readFile() {
+        String fileName = "myfile";
+        FileInputStream fileInputStream;
+        try {
+            fileInputStream = openFileInput(fileName);
+            byte[] buff = new byte[30];
+            int readLen = fileInputStream.read(buff);
+            Log.i("readFile", new String(buff));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_file_training, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
