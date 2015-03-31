547785b9bfb2838dbe60c75dcc3928fd8b85f10d
knightingal <yujianming1985@126.com>
2015-03-31 23:27:34 +0800

    add network

--- app/src/main/java/com/example/jianming/myapplication/FileTrainingActivity.java
+++ app/src/main/java/com/example/jianming/myapplication/FileTrainingActivity.java
@@ -2,6 +2,9 @@ package com.example.jianming.myapplication;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,29 +17,112 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
-public class FileTrainingActivity extends Activity {
+public class FileTrainingActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.read_file:
+                readFile();
+                break;
+            case R.id.write_file:
+                writeFile();
+                break;
+            case R.id.network:
+                network();
+                break;
+            default:
+                break;
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_training);
-        Button writeButton = (Button) findViewById(R.id.write_file);
-        writeButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                writeFile();
-            }
-        });
-        Button readButton = (Button) findViewById(R.id.read_file);
-        readButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readFile();
-            }
-        });
+
+        findViewById(R.id.write_file).setOnClickListener(this);
+        findViewById(R.id.read_file).setOnClickListener(this);
+        findViewById(R.id.network).setOnClickListener(this);
     }
+
+    private void network() {
+        String stringUrl = "http://192.168.0.100:8081/picDirs/picIndexAjax";
+        ConnectivityManager connMgr = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            Log.i("network", "No network connection available.");
+        }
+    }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Log.i("network", s);
+            Log.i("network", "" + s.length());
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        int len = 500;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+
+            int response = conn.getResponseCode();
+            Log.d("network", "The response is: " + response);
+            is = conn.getInputStream();
+            return readIt(is, len);
+
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String readIt(InputStream is, int len) throws IOException {
+        Reader reader = null;
+        reader = new InputStreamReader(is, "UTF-8");
+        char[] buffer = new char[len];
+        String content = "";
+        int readLen;
+        do {
+            readLen = reader.read(buffer);
+            content += new String(buffer).substring(0, readLen);
+        } while (readLen == len);
+        return content;
+    }
+
 
     private void writeFile() {
         String fileName = "myfile";
@@ -95,4 +181,6 @@ public class FileTrainingActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
