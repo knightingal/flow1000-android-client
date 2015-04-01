2d4872e754a881bbfcad4afd8b7138aa44a1426d
knightingal <yujianming1985@126.com>
2015-04-01 21:15:57 +0800

    commit from coding webIDE

--- app/src/main/java/com/example/jianming/myapplication/ZoomActivity.java
+++ app/src/main/java/com/example/jianming/myapplication/ZoomActivity.java
@@ -21,9 +21,7 @@ public class ZoomActivity extends Activity {
     // so that it can be canceled mid-way.
     private Animator mCurrentAnimator;
 
-    // The system "short" animation time duration, in milliseconds. This
-    // duration is ideal for subtle animations or animations that occur
-    // very frequently.
+    
     private int mShortAnimationDuration;
 
     @Override
