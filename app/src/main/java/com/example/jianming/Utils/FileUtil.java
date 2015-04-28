f213dac0873d19161cfeddd022afeafda804a31c
knightingal <yujianming1985@126.com>
2015-04-29 00:00:25 +0800

    new FileUtil

---
+++ app/src/main/java/com/example/jianming/Utils/FileUtil.java
@@ -0,0 +1,31 @@
+package com.example.jianming.Utils;
+
+import android.content.Context;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+
+/**
+ * Created by Jianming on 2015/4/28.
+ */
+public class FileUtil {
+
+
+    public static File getAlbumStorageDir(Context context, String albumName) {
+        String TAG = "FileUtil";
+
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_DOWNLOADS), albumName);
+        if (file.mkdirs()) {
+            Log.i(TAG, "Directory of " + file.getAbsolutePath() + " created");
+        }
+        return file;
+    }
+
+    public static boolean checkDirExist(Context context, String albumName) {
+        File file = new File(context.getExternalFilesDir(
+                Environment.DIRECTORY_DOWNLOADS), albumName);
+        return file.exists();
+    }
+}
