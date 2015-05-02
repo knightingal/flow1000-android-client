83e3a956772b1ff8f14d662e172c047076937c38
knightingal <yujianming1985@126.com>
2015-05-02 23:45:28 +0800

    big refactoring! remove some boring page.

--- app/src/main/java/com/example/jianming/myapplication/MainActivity.java
+++ app/src/main/java/com/example/jianming/myapplication/MainActivity.java
@@ -28,58 +28,26 @@ public class MainActivity extends Activity {
 
 
 
-        final Button mButton1 = (Button) findViewById(R.id.button1);
-        mButton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                self.startActivity(new Intent(self, ImageTraningActivity.class));
-            }
-        });
 
-        final Button mButton2 = (Button) findViewById(R.id.button2);
-        mButton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                self.startActivity(new Intent(self, ScreenSlidePagerActivity.class));
-            }
-        });
-        final Button mButton3 = (Button) findViewById(R.id.button3);
-        mButton3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                self.startActivity(new Intent(self, CrossActivity.class));
-            }
-        });
-        final Button mButton4 = (Button) findViewById(R.id.button4);
-        mButton4.setOnClickListener(new View.OnClickListener(){
+
+
+        final Button xrxBtn = (Button) findViewById(R.id.xrxBtn);
+        xrxBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 self.startActivity(new Intent(self, XrxActivity.class));
             }
         });
-        final Button mButton5 = (Button) findViewById(R.id.button5);
-        mButton5.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                self.startActivity(new Intent(self, ZoomActivity.class));
-            }
-        });
-        final Button mButton6 = (Button) findViewById(R.id.button6);
-        mButton6.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                self.startActivity(new Intent(self, CardFlipActivity.class));
-            }
-        });
-        final Button mButton7 = (Button) findViewById(R.id.button7);
-        mButton7.setOnClickListener(new View.OnClickListener(){
+
+        final Button forListBtn = (Button) findViewById(R.id.forListBtn);
+        forListBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 self.startActivity(new Intent(self, Activity4List.class));
             }
         });
-        final Button mButton8 = (Button) findViewById(R.id.button8);
-        mButton8.setOnClickListener(new View.OnClickListener(){
+        final Button fileTrainingBtn = (Button) findViewById(R.id.fileTrainingBtn);
+        fileTrainingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 self.startActivity(new Intent(self, FileTrainingActivity.class));
