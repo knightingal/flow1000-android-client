c96a6b4d9dce767c9503613be5d43a1d0f7ca19b
knightingal <yujianming1985@126.com>
2015-10-21 22:17:32 +0800

    change settings page to md style

--- app/src/main/java/com/example/jianming/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/jianming/myapplication/SettingActivity.java
@@ -2,46 +2,61 @@ package com.example.jianming.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
 import com.example.jianming.Utils.EnvArgs;
 
-
-public class SettingActivity extends Activity implements View.OnClickListener{
-
-
-    private EditText ipEditText;
-    private EditText portEditText;
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.button_done:
-                String ip = ipEditText.getText().toString();
-                String port = portEditText.getText().toString();
-                EnvArgs.serverIP = ip;
-                EnvArgs.serverPort = port;
-                finish();
-                //this.finishActivity(0);
-                break;
-            default:
-                break;
-        }
+public class SettingActivity extends AppCompatActivity{
+
+    @Bind(R.id.ip_edit)
+    public EditText ipEditText;
+
+    @Bind(R.id.port_edit)
+    public EditText portEditText;
+
+    @Bind(R.id.tl_custom)
+    Toolbar toolbar;
+
+    @OnClick(R.id.button_done)
+    public void doDoneBtn() {
+        String ip = ipEditText.getText().toString();
+        String port = portEditText.getText().toString();
+        EnvArgs.serverIP = ip;
+        EnvArgs.serverPort = port;
+        finish();
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
-        ipEditText = (EditText) findViewById(R.id.ip_edit);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         ipEditText.setFocusable(true);
         ipEditText.setFocusableInTouchMode(true);
         ipEditText.requestFocus();
         ipEditText.requestFocusFromTouch();
-        portEditText = (EditText) findViewById(R.id.port_edit);
-        findViewById(R.id.button_done).setOnClickListener(this);
     }
 
 
