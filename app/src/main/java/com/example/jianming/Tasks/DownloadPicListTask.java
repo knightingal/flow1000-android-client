9b8abe905ecda81debe0829d236cf81d3a4d62bd
Knightingal <yujianming1985@hotmail.com>
2017-06-08 22:50:46 +0800

    remove trash code

--- app/src/main/java/com/example/jianming/Tasks/DownloadPicListTask.java
+++ app/src/main/java/com/example/jianming/Tasks/DownloadPicListTask.java
@@ -12,35 +12,21 @@ import com.example.jianming.Utils.FileUtil;
 import com.example.jianming.beans.AlbumInfoBean;
 import com.example.jianming.beans.PicAlbumBean;
 import com.example.jianming.beans.PicInfoBean;
-import com.example.jianming.services.DownloadService;
-import com.example.jianming.views.DownloadProcessBar;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Jianming on 2015/5/4.
- */
 public class DownloadPicListTask extends DownloadWebpageTask{
     private static final String TAG = "DownloadPicListTask";
     private String dirName;
     private int index;
     private Context context;
 
-    public static void executeDownloadAlbumInfo(Context context, int serverIndex,  String dirName, String url) {
-        DownloadPicListTask task = new DownloadPicListTask(context, serverIndex,  dirName);
-
-        task.execute(url);
-    }
 
     /**
      * Constructor of DownloadPicListTask
--- app/src/main/java/com/example/jianming/services/DownloadService.java
+++ app/src/main/java/com/example/jianming/services/DownloadService.java
@@ -6,10 +6,6 @@ import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
-import com.example.jianming.Tasks.DownloadPicListTask;
-import com.example.jianming.beans.PicAlbumBean;
-import com.example.jianming.myapplication.PicCompletedListener;
-import com.example.jianming.views.DownloadProcessBar;
 
 public class DownloadService extends Service {
     private static final String TAG = "DownloadService";
@@ -19,34 +15,6 @@ public class DownloadService extends Service {
     public DownloadService() {
     }
 
-    public void startDownload(int serverIndex, String name, DownloadProcessBar downloadProcessView, String url) {
-        DownloadPicListTask.executeDownloadAlbumInfo(
-                this,
-                serverIndex,
-                name,
-                url
-        );
-    }
-
-    public void setPicCompletedListener(PicCompletedListener picCompletedListener) {
-        this.picCompletedListener = picCompletedListener;
-    }
-
-    private PicCompletedListener picCompletedListener = null;
-
-    public DownloadProcessBar getDownloadProcessBarByIndex(int index, int localPosition) {
-        if (this.picCompletedListener == null) {
-            return null;
-        }
-        return this.picCompletedListener.getDownloadProcessBarByIndex(index, localPosition);
-    }
-
-    public void doPicListDownloadComplete(String dirName, int index, int localPosition) {
-        PicAlbumBean.setExistByServerIndex(index, 1);
-        if (picCompletedListener != null) {
-            picCompletedListener.doPicListDownloadComplete(dirName, index, localPosition);
-        }
-    }
 
     public class LocalBinder extends Binder {
         public DownloadService getService() {
@@ -60,9 +28,7 @@ public class DownloadService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         Log.d(TAG, "onBind");
-//        throw new UnsupportedOperationException("Not yet implemented");
         return mBinder;
     }
 
@@ -70,35 +36,9 @@ public class DownloadService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d(TAG, "onCreate");
-//        startCounter();
 
     }
 
-    Thread th;
-
-    private void startCounter() {
-        th = new Thread(r);
-        running = true;
-        th.start();
-    }
-
-    public void startDownload() {
-        Log.d(TAG, "startDownload");
-    }
-
-    Runnable r = new Runnable() {
-        @Override
-        public void run() {
-            while(running) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Log.d(TAG, "thread report!");
-            }
-        }
-    };
 
     boolean running;
 
@@ -107,11 +47,6 @@ public class DownloadService extends Service {
         super.onDestroy();
         Log.d(TAG, "onDestroy");
         running = false;
-//        try {
-//            th.join(2000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
     }
 
     @Override
