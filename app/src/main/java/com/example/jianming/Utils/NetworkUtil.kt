685878f558a1407fbf7dbeb376010d9d59249506
knightingal <yujianming1985@hotmail.com>
2023-08-30 23:28:24 +0800

    fix lint

--- app/src/main/java/com/example/jianming/Utils/NetworkUtil.kt
+++ app/src/main/java/com/example/jianming/Utils/NetworkUtil.kt
@@ -8,12 +8,8 @@ object NetworkUtil {
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-        return if (connectivityManager != null) {
-            val networkInfo = connectivityManager.activeNetworkInfo
-            networkInfo != null && networkInfo.isConnected
-        } else {
-            false
-        }
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
     }
 
     val okHttpClient = OkHttpClient()
