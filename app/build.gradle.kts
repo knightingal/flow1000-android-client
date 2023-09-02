bae95ac18b6b53d91526367499705ce40a5bcf5c
knightingal <yujianming1985@hotmail.com>
2023-09-02 16:54:49 +0800

    update androidx.core:core-ktx and com.google.android.material:material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     implementation("com.nostra13.universalimageloader:universal-image-loader:1.9.5")
 
 
-    var jacksonVersion = "2.15.2"
+    val jacksonVersion = "2.15.2"
     implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.11")
@@ -89,9 +89,9 @@ dependencies {
     implementation( "com.google.code.gson:gson:2.10.1")
 
     implementation("com.google.guava:guava:32.0.1-android")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
