876535c2f1b00c39607d6a063d350febb5f7a67e
knightingal <yujianming1985@hotmail.com>
2023-10-30 23:07:46 +0800

    about activity to kt

--- app/src/main/java/com/example/jianming/myapplication/AboutActivity.kt
+++ app/src/main/java/com/example/jianming/myapplication/AboutActivity.kt
@@ -1,50 +1,30 @@
-package com.example.jianming.myapplication;
+package com.example.jianming.myapplication
 
-import static android.content.pm.PackageManager.GET_META_DATA;
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.progressindicator.CircularProgressIndicator
 
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.progressindicator.CircularProgressIndicator;
-
-public class AboutActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        CircularProgressIndicator progressIndicator = findViewById(R.id.download_process1);
-        TextView versionCodeText = findViewById(R.id.version_code);
-        TextView versionNameText = findViewById(R.id.version_name);
-
-        ImageView imageView = findViewById(R.id.image_view_logo);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                progressIndicator.setProgressCompat(50, true);
-                progressIndicator.setMax(200);
-            }
-        });
-
-        PackageManager packageManager = getPackageManager();
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        val progressIndicator = findViewById<CircularProgressIndicator>(R.id.download_process1)
+        val versionCodeText = findViewById<TextView>(R.id.version_code)
+        val versionNameText = findViewById<TextView>(R.id.version_name)
+        val imageView = findViewById<ImageView>(R.id.image_view_logo)
+        imageView.setOnClickListener { }
+        val packageManager = packageManager
         try {
-            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
-            String versionName = packageInfo.versionName;
-            versionNameText.setText(versionName);
-            long versionCode = packageInfo.getLongVersionCode();
-            versionCodeText.setText(String.valueOf(versionCode));
-
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
+            val packageInfo = packageManager.getPackageInfo(packageName, 0)
+            val versionName = packageInfo.versionName
+            versionNameText.text = versionName
+            val versionCode = packageInfo.longVersionCode
+            versionCodeText.text = versionCode.toString()
+        } catch (e: PackageManager.NameNotFoundException) {
+            throw RuntimeException(e)
         }
-
     }
 }
