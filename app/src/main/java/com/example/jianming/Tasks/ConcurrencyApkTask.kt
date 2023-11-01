399d7fe1eba3f1a0826e7f828a41942414737f32
knightingal <yujianming1985@hotmail.com>
2023-11-01 23:46:32 +0800

    install apk

---
+++ app/src/main/java/com/example/jianming/Tasks/ConcurrencyApkTask.kt
@@ -0,0 +1,51 @@
+package com.example.jianming.Tasks
+
+import android.graphics.BitmapFactory
+import android.util.Log
+import com.example.jianming.util.Decryptor
+import com.example.jianming.util.NetworkUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Request
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+
+object ConcurrencyApkTask {
+    private const val TAG = "DLImageTask"
+    fun downloadUrl(src: String, dest: File, callback: (bytes: ByteArray) -> Unit): Unit {
+        MainScope().launch {
+            val bytes = makeRequest(src, dest)
+            if (bytes != null) {
+                callback(bytes)
+            }
+        }
+    }
+
+    suspend fun makeRequest(src: String, dest: File): ByteArray? {
+        return withContext(Dispatchers.IO) {
+            Log.d(TAG, "start download $src")
+            val request = Request.Builder().url(src).build()
+            var bytes: ByteArray?
+            while (true) {
+                try {
+                    bytes = NetworkUtil.okHttpClient.newCall(request).execute().body.bytes()
+                    val options: BitmapFactory.Options = BitmapFactory.Options()
+                    options.inJustDecodeBounds = true
+                    val fileOutputStream = FileOutputStream(dest, true)
+                    fileOutputStream.write(bytes)
+                    fileOutputStream.close()
+                    break
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    Log.e(TAG, "download $src error")
+                }
+            }
+            bytes
+
+        }
+
+    }
+}
--- app/src/main/java/com/example/jianming/myapplication/AboutActivity.kt
+++ app/src/main/java/com/example/jianming/myapplication/AboutActivity.kt
@@ -2,19 +2,27 @@ package com.example.jianming.myapplication
 
 import SERVER_IP
 import SERVER_PORT
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.Settings
 import android.util.Log
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
+import com.example.jianming.Tasks.ConcurrencyApkTask
 import com.example.jianming.Tasks.ConcurrencyJsonApiTask
 import com.example.jianming.beans.ApkConfig
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
+import java.io.File
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -45,10 +53,50 @@ class AboutActivity : AppCompatActivity() {
                 Log.d("about", apkConfig.toString())
                 if (apkConfig.versionCode > versionCode) {
                     Toast.makeText(this@AboutActivity, "you have newer apk", Toast.LENGTH_LONG).show()
+                    val directory = File(this@AboutActivity.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "apk")
+                    apkFile = File(directory, apkConfig.apkName)
+                    directory.mkdirs()
+                    ConcurrencyApkTask.makeRequest(apkConfig.downloadUrl, apkFile)
+
+                    val intent = Intent(
+                        Settings.ACTION_MANAGE_UNKNOWN_APP_SOURCES, Uri.parse(
+                            "package:$packageName"
+                        )
+                    )
+                    startActivityForResult(intent, 1)
                 } else {
                     Toast.makeText(this@AboutActivity, "you are in newest apk", Toast.LENGTH_LONG).show()
                 }
             }
+        }
+    }
+
+    lateinit var apkFile: File
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == 0) {
+            if (requestCode == 1) {
+                openAPKFile()
+            }
+        }
+    }
+
+    private fun openAPKFile() {
+        val mimeDefault = "application/vnd.android.package-archive"
+        try {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            //兼容7.0
+            val contentUri = FileProvider.getUriForFile(
+                this,
+                packageName + ".fileprovider", apkFile
+            )
+            intent.setDataAndType(contentUri, mimeDefault)
+                //如果APK安装界面存在，携带请求码跳转。使用forResult是为了处理用户 取消 安装的事件。外面这层判断理论上来说可以不要，但是由于国内的定制，这个加上还是比较保险的
+            startActivityForResult(intent, 2)
+        } catch (e: Throwable) {
+            e.printStackTrace()
         }
     }
 }
