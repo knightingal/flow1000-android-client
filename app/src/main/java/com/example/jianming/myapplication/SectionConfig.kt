45037c58893afde509a3ec246b34f310d225dee9
knightingal <yujianming1985@hotmail.com>
2024-03-22 23:28:09 +0800

    fix 1806 album config

--- app/src/main/java/com/example/jianming/myapplication/SectionConfig.kt
+++ app/src/main/java/com/example/jianming/myapplication/SectionConfig.kt
@@ -5,12 +5,14 @@ class SectionConfig(var name: String, var encryped: Boolean, var baseUrl: String
 val SECTION_CONFIG_MAP: Map<String, SectionConfig> = initSectionConfig();
 
 fun initSectionConfig(): Map<String, SectionConfig> {
+    // TODO: so joke, the album config is hard coded here!!!
     return mapOf(
         "flow1000" to SectionConfig("flow1000", true, "encrypted"),
         "ship" to SectionConfig("ship", true, "encrypted"),
         "1803" to SectionConfig("1803", false, "1803"),
         "1804" to SectionConfig("1804", false, "1804"),
         "1805" to SectionConfig("1805", false, "1805"),
+        "1806" to SectionConfig("1806", false, "1806"),
     );
 }
 
