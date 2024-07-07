2ed7cf197008b658ad94f56dc8b94235248ecdb8
knightingal <yujianming1985@hotmail.com>
2024-07-07 10:08:31 +0800

    add finishCounter map

--- app/src/main/java/com/example/jianming/services/ProcessCounter.kt
+++ app/src/main/java/com/example/jianming/services/ProcessCounter.kt
@@ -4,6 +4,7 @@ import java.util.concurrent.ConcurrentHashMap
 
 object ProcessCounter {
     private val counter:ConcurrentHashMap<Long, Counter> = ConcurrentHashMap<Long, Counter>()
+    private val finishedCounter = mutableMapOf<Long, Counter>()
 
     public fun initCounter(id: Long, max:Int): Counter? {
         return counter.putIfAbsent(id, Counter(max))
@@ -17,10 +18,17 @@ object ProcessCounter {
     }
 
     public fun getCounter(id: Long): Counter? {
-        return counter[id]
+        val counter1 = counter[id]
+        if (counter1 != null) {
+            return counter1
+        }
+        return finishedCounter[id]
     }
 
     public fun remove(id: Long) {
-        counter.remove(id)
+        val counter1 = counter.remove(id)
+        if (counter1 != null) {
+            finishedCounter[id] = counter1
+        }
     }
 }
