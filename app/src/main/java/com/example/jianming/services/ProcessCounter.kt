8d86efd311d9526844d7051dc2c40b9faa9576a0
knightingal <yujianming1985@hotmail.com>
2024-07-09 00:02:32 +0800

    fix lint

--- app/src/main/java/com/example/jianming/services/ProcessCounter.kt
+++ app/src/main/java/com/example/jianming/services/ProcessCounter.kt
@@ -7,18 +7,18 @@ object ProcessCounter {
     private val counter:ConcurrentHashMap<Long, Counter> = ConcurrentHashMap<Long, Counter>()
     private val finishedCounter = mutableMapOf<Long, Counter>()
 
-    public fun initCounter(id: Long, max:Int): Counter? {
+    fun initCounter(id: Long, max:Int): Counter? {
         return counter.putIfAbsent(id, Counter(max))
     }
 
-    public fun addCounter(id: Long): Counter? {
-        return counter.computeIfPresent(id) { inId, inCount ->
+    fun addCounter(id: Long): Counter? {
+        return counter.computeIfPresent(id) { _, inCount ->
             inCount.add()
             inCount
         }
     }
 
-    public fun getCounter(id: Long): Counter? {
+    fun getCounter(id: Long): Counter? {
         val counter1 = counter[id]
         if (counter1 != null) {
             return counter1
@@ -26,7 +26,7 @@ object ProcessCounter {
         return finishedCounter[id]
     }
 
-    public fun remove(id: Long) {
+    fun remove(id: Long) {
         val counter1 = counter.remove(id)
         if (counter1 != null) {
             finishedCounter[id] = counter1
@@ -35,10 +35,6 @@ object ProcessCounter {
 
     class Counter(val max: Int) {
         private val process: AtomicInteger = AtomicInteger(0)
-
-        fun setProcess(value: Int) {
-            process.set(value)
-        }
 
         fun add() {
             process.incrementAndGet()
