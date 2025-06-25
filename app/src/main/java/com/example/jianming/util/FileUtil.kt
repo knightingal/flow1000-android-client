fbc96bce36a7352b3909a0a75b0dd81ed73c533a
knightingal <yujianming1985@hotmail.com>
2025-06-25 22:49:54 +0800

    refactor FileUtil from java to kt

--- app/src/main/java/com/example/jianming/util/FileUtil.kt
+++ app/src/main/java/com/example/jianming/util/FileUtil.kt
@@ -1,45 +1,53 @@
-package com.example.jianming.util;
+package com.example.jianming.util
 
-import android.content.Context;
-import android.os.Environment;
-import android.util.Log;
-
-import java.io.File;
+import android.content.Context
+import android.os.Environment
+import android.util.Log
+import java.io.File
 
 /**
  * Created by Jianming on 2015/4/28.
  */
-public class FileUtil {
-    private static final String TAG = "FileUtil";
+object FileUtil {
+    private const val TAG = "FileUtil"
 
-    public static File getSectionStorageDir(Context context, String sectionName) {
-        File file = new File(context.getExternalFilesDir(
-                Environment.DIRECTORY_DOWNLOADS), sectionName);
+    fun getSectionStorageDir(context: Context, sectionName: String): File {
+        val file = File(
+            context.getExternalFilesDir(
+                Environment.DIRECTORY_DOWNLOADS
+            ), sectionName
+        )
         if (file.mkdirs()) {
-            Log.i(TAG, "Directory of " + file.getAbsolutePath() + " created");
+            Log.i(TAG, "Directory of " + file.absolutePath + " created")
         }
-        return file;
+        return file
     }
 
-    public static void removeDir(Context context, String sectionName) {
-        File file = new File(context.getExternalFilesDir(
-                Environment.DIRECTORY_DOWNLOADS), sectionName
-        );
+    @JvmStatic
+    fun removeDir(context: Context, sectionName: String) {
+        val file = File(
+            context.getExternalFilesDir(
+                Environment.DIRECTORY_DOWNLOADS
+            ), sectionName
+        )
         try {
-            File[] files = file.listFiles();
-            for (File imgFile : files) {
-                imgFile.delete();
+            val files = file.listFiles()
+            for (imgFile in files!!) {
+                imgFile.delete()
             }
-            file.delete();
-        } catch (Exception e) {
-            e.printStackTrace();
+            file.delete()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 
-    public static boolean checkDirExist(Context context, String sectionName) {
-        File file = new File(context.getExternalFilesDir(
-                Environment.DIRECTORY_DOWNLOADS), sectionName);
+    fun checkDirExist(context: Context, sectionName: String): Boolean {
+        val file = File(
+            context.getExternalFilesDir(
+                Environment.DIRECTORY_DOWNLOADS
+            ), sectionName
+        )
 
-        return file.exists();
+        return file.exists()
     }
 }
