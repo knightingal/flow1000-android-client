d28d6ca2b2492f6afac1ccf0824ffc11ed0ddf5e
knightingal <yujianming1985@hotmail.com>
2025-10-23 21:42:49 +0800

    create ut for okhttp client

---
+++ app/src/test/java/org/knightingal/flow1000/client/OkHttpClientTest.kt
@@ -0,0 +1,34 @@
+package org.knightingal.flow1000.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.request.get
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.Dispatcher
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.internal.wait
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class OkHttpClientTest {
+
+    @Test
+    fun clientTest() {
+        val client = OkHttpClient()
+        val request = Request
+                .Builder()
+                .url("http://localhost:8082")
+                .build()
+        val body = client.newCall(request).execute().body!!.string()
+        println(body)
+
+
+    }
+}
